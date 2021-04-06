d8b664f73dfde88a1d08ae2dd21a1417eb4325a0
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-06 15:45:56 -0700

    Navigation initial draft

---
+++ public/components/Navigation.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import NavTabs from "./navBar.js";
+import Introduction from './Introduction.js'
+import Project from './Project'
+import Contact from './Contact'
+import Header from './Header'
+import Footer from './Footer'
+
+function Navigation() {
+  const [currentPage, setCurrentPage] = React.setState("")
+
+  function handlePageChange(page) {
+    setCurrentPage(page)
+  };
+
+return (
+    <>
+    <Header />
+        {currentPage === "" ? <Introduction /> : <></>}
+        {currentPage === "Resume" ? <Resume /> : <></>}
+        {currentPage === "Project" ? <Project /> : <></>} 
+        {currentPage === "Contact" ? <Contact /> : <></>} 
+    <Footer />
+    </>
+)
+
+}
+
+export default Navigation;
