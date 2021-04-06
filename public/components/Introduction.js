2f8d47a31bbb62aaa6fb197964686d378bcf5ddf
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-06 15:36:56 -0700

    blank components

---
+++ public/components/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Introduction() {
+    return (
+        <>
+        <p>Erik Portillo</p>
+        <p>Please Select from one of the following...</p>
+        </>
+    )
+}
+
+export default Introduction
