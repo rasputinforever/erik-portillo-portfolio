3e7e7f5ab71c3971de06fa5f767ad890751624cd
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-07 23:48:34 -0700

    images linked and rendered

--- src/util/projectDB.js
+++ src/util/projectDB.js
@@ -3,7 +3,7 @@ const projects = [
         name: 'Castaway: Flight of the Phoenix',
         deployed: 'https://cyoa-rpg.herokuapp.com/',
         git: 'git.com',
-        image: 'castaway.PNG',
+        image: '/images/castaway.JPG',
         description: 'A Choose-Your-Own-Adventure game utilizing Node.JS, Express, Express-Handlebars, and Sequelize',
         type: 'group'
     },
@@ -11,21 +11,21 @@ const projects = [
         name: 'Auto Compli-Mentor',
         deployed: 'https://main.d3l3xweb6e8q2c.amplifyapp.com/',
         git: 'https://github.com/rasputinforever/auto-complimentor',
-        image: 'mentor.PNG',
+        image: '/images/mentors.JPG',
         description: 'A React based application that gives the user automated mentorship.',
         type: 'solo'
     },
     {
         name: 'Employee Manager',
         git: 'https://github.com/rasputinforever/Employee-Manager',
-        image: 'employee.PNG',
+        image: '/images/employee.JPG',
         description: 'Terminal application using Node JS, Inquirer to manage a MySQL database.',
         type: 'solo'
     },
     {
         name: 'Auto ReadMe',
         git: 'https://github.com/rasputinforever/Auto-ReadMe',
-        image: 'readme.PNG',
+        image: '/images/readme.PNG',
         description: 'Terminal application using Node JS, Inquirer to create and edit README.md files.',
         type: 'solo'
     },
@@ -33,15 +33,15 @@ const projects = [
         name: 'My Weather Dashboard',
         deployed: 'https://rasputinforever.github.io/My-Weather-Dashboard/',
         git: 'https://github.com/rasputinforever/My-Weather-Dashboard',
-        image: 'weather.PNG',
+        image: '/images/weather.JPG',
         description: 'Website utilizing 3rd party APIs to generate a weather report for your area. Local storage used to store user history..',
         type: 'solo'
     },
     {
-        name: 'My Weather Dashboard',
+        name: 'Quiz Game',
         deployed: 'https://rasputinforever.github.io/Quiz-Game/',
         git: 'https://github.com/rasputinforever/Quiz-Game',
-        image: 'quiz.PNG',
+        image: '/images/quiz.JPB',
         description: 'JavaScript based Quiz Game utlizing localStorage, setTimeout, JQuery, and DOM manipulation.',
         type: 'solo'
     }
