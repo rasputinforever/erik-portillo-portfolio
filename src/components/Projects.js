cdea96acb55d83c4dddfc488d84013c826d81539
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-08 15:19:32 -0700

    grid is responsiv

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import Project from './Project'
 
 import ProjectDB from '../util/projectDB.js'
 
-import { Grid, Segment, Header, Icon } from 'semantic-ui-react'
+import { Grid, Segment, Card, Header, Icon } from 'semantic-ui-react'
 
 function Projects(){
     
@@ -16,26 +16,29 @@ function Projects(){
   }, [])
 
   const renderedProjects = projects.map((proj) => {
-    return <Project name={proj.name} type={proj.type} dep={proj.deployed} git={proj.git} image={proj.image} description={proj.description}/>
+    return (
+      <Card.Group itemsPerRow={1}>
+    <Project name={proj.name} type={proj.type} dep={proj.deployed} git={proj.git} image={proj.image} description={proj.description}/>
+          </Card.Group>
+    )
   })
 
     return (
         <section>
-          <Header as='h2' textAlign='center'>
+          <Header id="Projects" as='h2' textAlign='center'>
             Projects
             <Header.Subheader>
               <Icon name="code" />Group (red, chess) and Solo Projects (green, pawn)
             </Header.Subheader>
           </Header>
+          <Grid stackable columns={3}>
+        {renderedProjects}
 
-          <Grid stackable columns={projects.length}>
           <Grid.Column>
-          <Segment>
-          {renderedProjects}
-          </Segment>
-          </Grid.Column>
-          </Grid>
-
+      
+    </Grid.Column>
+  </Grid>
+          
         </section>
     )
 }
