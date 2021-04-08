ddd58c14e33cec1f5d81c3e191fb3631b6b62b8e
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-07 23:36:37 -0700

    adds links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,9 @@ function Contact() {
         <>
         <h2>Contact</h2>
         <ul>
-            <li>GitHub</li>
-            <li>LinkedIN</li>
-            <li>Email</li>
+            <li><a href="https://github.com/rasputinforever">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/erik-portillo-1216a926/">LinkedIn</a></li>
+            <li><a href="mailto: erik.justin.portillo@gmail.com">erik.justin.portillo@gmail.com</a></li>
         </ul>
         </>
     )
