580dda1c282b08369ad63d41581b204310856cde
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-08 14:20:04 -0700

    cleanup

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Icon, Image } from 'semantic-ui-react'
+import { Header, Image } from 'semantic-ui-react'
 
 function Introduction() {
     return (  
@@ -17,7 +17,3 @@ function Introduction() {
 }
 
 export default Introduction
-
-// <h1>Erik Portillo</h1>
-// <h2>Full-Stack Developer, Chemist, Lover of Cats</h2>
-// <p>Problem solving has never been so productive. Check out my projects, resume, and if you're at all interested feel free to contact me.</p>
