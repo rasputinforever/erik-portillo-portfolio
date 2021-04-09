5287757f7e0ae5eda5d28774e9d274c13250282e
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-08 18:39:22 -0700

    more introductory text

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -9,8 +9,11 @@ function Introduction() {
         <Image circular src='/images/erik.png' /> 
         <Header.Content>Erik Portillo</Header.Content>
             <Header.Subheader>
-              <Icon name="bullhorn" />Full-Stack Developer, Chemist, Lover of Cats
+              <Icon name="bullhorn" />Full-Stack Developer, Chemist, Lover of Cats. I've been developing and programming applications professionally since 2019, starting humbly with Excel's integrated VBA, then graduating to JavaScript in 2020. My time at Univeristy of Oregon's Developer BootCamp has prepared me for modern web development. Utlizing Node, Express, and React, the lines between "website" and "application" begin to blur.
+
             </Header.Subheader>
+            <br></br>
+            <Header.Subheader>I hope you enjoy your time here.</Header.Subheader>
           </Header>
     </section>
     )
